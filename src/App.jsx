af7a3c5f034f5c840830805d3786a29446cdfc67
Cong Nhut <dgxa9999@gmail.com>
2024-03-30 10:45:55 +0700

    1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Home from "./pages"
+import { GlobalStyle } from "./components/global-style"
+
+function App() {
+  return (
+    <GlobalStyle >
+      <Home />
+    </GlobalStyle>
+  )
+}
+
+export default App
